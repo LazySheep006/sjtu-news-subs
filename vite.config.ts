72172733dfce3d5ac53016e0bd98cac96602fcc3
LazySheep006 <3129584372@qq.com>
2026-01-17 22:55:27 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: If you are deploying to https://<USERNAME>.github.io/<REPO>/
-  // Change the line below to: base: '/<REPO>/',
-  // Example: if your repo is called 'sjtu-news', set base: '/sjtu-news/'
-  base: './', 
+  base: '/sjtu-news-subs', 
 })
